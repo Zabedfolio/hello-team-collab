86fe428233fd9ce6b1a084d74f7ef3c3228f85ac
Zabedfolio <zabedfolio@gmail.com>
2026-09-12 07:55:27 +0600

    conflict resolved

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
+document.getElementById("timeMsg").innerText = "Hello from Team Updated!";
+console.log("Team Project"); 
 
- document.getElementById("timeMsg").innerText = "Hello from Team Updated!";
- console.log("Team Project"); 
-document.getElementById("greet").innerText = "Hello from Team!"
-console.log("Team Project");
